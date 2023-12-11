38fa1d0a0b68e84f4e3d916ae9719013e097930b
HalDaiva <daivahalilintar@gmail.com>
2023-12-11 19:08:10 +0700

    tambah tabel baru specialist

---
+++ database/editserviceit2.sql
@@ -0,0 +1,23 @@
+-- MEMBUAT TABEL SPECIALIST
+CREATE TABLE serviceit.specialist(
+    ID_SPECIALIST smallint(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    NAMA_SPECIALIST varchar(255) NOT NULL
+);
+
+-- INPUT DATA PADA TABEL SPECIALIST
+INSERT INTO serviceit.specialist (NAMA_SPECIALIST)
+VALUES ('Laptop Hardware Specialist'),
+       ('Desktop Hardware Specialist'),
+       ('Windows Software Specialist'),
+       ('Mac Software Specialist'),
+       ('Linux Software Specialist'),
+       ('Gadget Specialist');
+
+-- ADD ID_SPECIALIST DI TABEL MECHANIC
+ALTER TABLE serviceit.mechanic ADD ID_SPECIALIST smallint(6);
+
+-- ADD FOREIGN KEY KE ID_SPECIALIST DI TABEL MECHANIC
+ALTER TABLE serviceit.mechanic
+    ADD FOREIGN KEY (ID_SPECIALIST) REFERENCES serviceit.specialist (ID_SPECIALIST);
+
+
