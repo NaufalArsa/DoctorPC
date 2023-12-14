60e608a108b0e1f5edc141d174aa2f1734c027a6
ArvinHendro45 <152869355+ArvinHendro45@users.noreply.github.com>
2023-12-14 18:33:47 +0700

    Update Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -17,11 +17,50 @@ class Controller {
         header("location: ../resources/dashboard.php");
     }
 
-    public function showrequest(){
-        $dataReq = $this->model->showrequest();
-        include("../resources/views/service-request.php");
+    public function handleRequest() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $action = $_POST['action'];
+
+            switch ($action) {
+                case 'deleteService':
+                    $serviceId = $_POST['serviceId'];
+                    $this->deleteService($serviceId);
+                    break;
+                case 'addRequest':
+                    $this->addRequest(
+                        $_POST['namaPelanggan'],
+                        $_POST['kontakPelanggan'],
+                        $_POST['merkDevice'],
+                        $_POST['statusService'],
+                        $_POST['deskripsi'],
+                        $_POST['idMechanic']
+                        );
+                    break;
+            }
+        }
     }
 
+    public function showrequest(){
+        return $this->model->showrequest();
+    }
+
+    public function deleteService($serviceId) {
+        $result = $this->model->deleteService($serviceId);
+        if ($result) {
+            // Redirect atau sesuaikan dengan kebutuhan setelah penghapusan berhasil
+            header("Location: ../resources/views/service-request.php");
+        } else {
+            // Handle jika penghapusan gagal
+            echo "Error deleting service.";
+        }
+    }
+
+    public function addRequest($namaPelanggan, $kontakPelanggan, $merkDevice, $statusService, $deskripsi, $idMechanic) {
+        $this->model->addRequest($namaPelanggan, $kontakPelanggan, $merkDevice, $statusService, $deskripsi, $idMechanic);
+        // Redirect atau sesuaikan dengan kebutuhan setelah penambahan berhasil
+        header("Location: ../resources/views/service-request.php");
+    }
+    
 
     // FITUR REVIEW [ALAM]
     public function addReview($userName, $reviewText) {
