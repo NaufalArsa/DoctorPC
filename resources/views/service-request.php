503749d55b194d6cf9d01906f2f63a9b83780ae2
ArvinHendro45 <152869355+ArvinHendro45@users.noreply.github.com>
2023-12-14 21:17:22 +0700

    Update service-request.php

--- resources/views/service-request.php
+++ resources/views/service-request.php
@@ -13,12 +13,142 @@ $dataReq = $controller->showrequest();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Service Request</title>
 </head>
 
 <body>
+
+    <nav class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                    <div class="hidden md:block">
+                        <div class="ml-5 flex items-baseline space-x-4">
+                            <a href="#" class="text-white rounded-md px-3 py-2 text-lg font-bold">SERVICE - IT</a>
+                            <!-- <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Reports</a> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="hidden md:block">
+                    <div class="  flex items-center">
+                        <!-- <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span class="absolute -inset-1.5"></span>
+                      <span class="sr-only">View notifications</span>
+                      <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                      </svg>
+                    </button> -->
+                        <h3 class="text-white block text-base font-medium">Hello,
+                            <?php
+
+                        if(isset($_SESSION['name'])) {
+                            $username = $_SESSION['name'];
+                        } else {
+                            $username = "Guest";
+                        }
+
+                        echo $username; ?></h3>
+
+                        <a href="#"><i
+                                class="ml-5 h-8 w-8 grid place-self-center p-2 pl-2.5 hover:text-gray-800 hover:bg-white hover:rounded-full hover:duration-700 fa-regular fa-user text-white"></i></a>
+                        <!-- <img class="ml-5 h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""> -->
+                        <!-- Profile dropdown -->
+                        <!-- <div class="relative ml-3">
+                      <div>
+                        <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                          <span class="absolute -inset-1.5"></span>
+                          <span class="sr-only">Open user menu</span>
+                        </button>
+                      </div>
+                      <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                      </div>
+                    </div> -->
+                    </div>
+                </div>
+                <div class="-mr-2 flex md:hidden">
+                    <!-- Mobile menu button -->
+                    <button type="button"
+                        class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="absolute -inset-0.5"></span>
+                        <span class="sr-only">Open main menu</span>
+                        <!-- Menu open: "hidden", Menu closed: "block" -->
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        <!-- Menu open: "block", Menu closed: "hidden" -->
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="md:hidden" id="mobile-menu">
+            <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                    aria-current="page">Dashboard</a>
+                <a href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+                <a href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                <a href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+                <a href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Reports</a>
+            </div>
+            <div class="border-t border-gray-700 pb-3 pt-4">
+                <div class="flex items-center px-5">
+                    <div class="flex-shrink-0">
+                        <img class="h-10 w-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt="">
+                    </div>
+                    <div class="ml-3">
+                        <div class="text-base font-medium leading-none text-white">Tom Cook</div>
+                        <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+                    </div>
+                    <button type="button"
+                        class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span class="absolute -inset-1.5"></span>
+                        <span class="sr-only">View notifications</span>
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="mt-3 space-y-1 px-2">
+                    <a href="#"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+                        Profile</a>
+                    <a href="#"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
+                    <a href="#"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
+                        out</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addRequestModal">
+        <button type="button" class="btn btn-primary bg-blue-500 hover:bg-blue-700 text-white" data-toggle="modal"
+            data-target="#addRequestModal">
             Tambah Request
         </button>
         <form class="form-inline">
@@ -58,47 +188,49 @@ $dataReq = $controller->showrequest();
             </tbody>
         </table>
     </div>
-    <div class="modal fade" id="addRequestModal" tabindex="-1" role="dialog" aria-labelledby="addRequestModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Form tambah request -->
-                <form id="addRequestForm">
-                    <div class="form-group">
-                        <label for="namaPelanggan">Nama Pelanggan:</label>
-                        <input type="text" class="form-control" id="namaPelanggan" name="namaPelanggan" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="kontakPelanggan">Kontak Pelanggan:</label>
-                        <input type="text" class="form-control" id="kontakPelanggan" name="kontakPelanggan" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="merkDevice">Merk Device:</label>
-                        <input type="text" class="form-control" id="merkDevice" name="merkDevice" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="statusService">Status Service:</label>
-                        <input type="text" class="form-control" id="statusService" name="statusService" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="deskripsi">Deskripsi:</label>
-                        <textarea class="form-control" id="deskripsi" name="deskripsi" required></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="idMechanic">ID Mechanic:</label>
-                        <input type="text" class="form-control" id="idMechanic" name="idMechanic" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Tambah Request</button>
-                </form>
+    <div class="modal fade" id="addRequestModal" tabindex="-1" role="dialog" aria-labelledby="addRequestModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form tambah request -->
+                    <form id="addRequestForm">
+                        <div class="form-group">
+                            <label for="namaPelanggan">Nama Pelanggan:</label>
+                            <input type="text" class="form-control" id="namaPelanggan" name="namaPelanggan" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="kontakPelanggan">Kontak Pelanggan:</label>
+                            <input type="text" class="form-control" id="kontakPelanggan" name="kontakPelanggan"
+                                required>
+                        </div>
+                        <div class="form-group">
+                            <label for="merkDevice">Merk Device:</label>
+                            <input type="text" class="form-control" id="merkDevice" name="merkDevice" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="statusService">Status Service:</label>
+                            <input type="text" class="form-control" id="statusService" name="statusService" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="deskripsi">Deskripsi:</label>
+                            <textarea class="form-control" id="deskripsi" name="deskripsi" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="idMechanic">ID Mechanic:</label>
+                            <input type="text" class="form-control" id="idMechanic" name="idMechanic" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-700 text-white">Tambah Request</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
     <script>
     $(document).ready(function() {
         // Handle search button click event
@@ -116,53 +248,56 @@ $dataReq = $controller->showrequest();
             });
         });
         $("#addRequestForm").submit(function(event) {
-        event.preventDefault();
+            event.preventDefault();
 
-        // Mengambil data dari form
-        var formData = {
-            action: 'addRequest',
-            namaPelanggan: $("#namaPelanggan").val(),
-            kontakPelanggan: $("#kontakPelanggan").val(),
-            merkDevice: $("#merkDevice").val(),
-            statusService: $("#statusService").val(),
-            deskripsi: $("#deskripsi").val(),
-            idMechanic: $("#idMechanic").val()
-        };
+            // Mengambil data dari form
+            var formData = {
+                action: 'addRequest',
+                namaPelanggan: $("#namaPelanggan").val(),
+                kontakPelanggan: $("#kontakPelanggan").val(),
+                merkDevice: $("#merkDevice").val(),
+                statusService: $("#statusService").val(),
+                deskripsi: $("#deskripsi").val(),
+                idMechanic: $("#idMechanic").val()
+            };
 
-        // Melakukan AJAX request
-        $.ajax({
-            type: 'POST',
-            url: '../../app/Controller.php',
-            data: formData,
-            success: function(response) {
-                // Reload halaman atau perbarui tabel setelah berhasil
-                location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-                alert('An error occurred while adding the request.');
-            }
+            // Melakukan AJAX request
+            $.ajax({
+                type: 'POST',
+                url: '../../app/Controller.php',
+                data: formData,
+                success: function(response) {
+                    // Reload halaman atau perbarui tabel setelah berhasil
+                    location.reload();
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                    alert('An error occurred while adding the request.');
+                }
+            });
         });
     });
-});
+
     function deleteService(serviceId) {
         if (confirm('Are you sure you want to delete this service?')) {
             $.ajax({
                 type: 'POST',
                 url: '../../app/Controller.php', // Replace with the actual path to your controller
-                data: { action: 'deleteService', serviceId: serviceId },
-                success: function (response) {
+                data: {
+                    action: 'deleteService',
+                    serviceId: serviceId
+                },
+                success: function(response) {
                     // Reload the page or update the table as needed
                     location.reload();
                 },
-                error: function (xhr, status, error) {
+                error: function(xhr, status, error) {
                     console.error(xhr.responseText);
                     alert('An error occurred while deleting the service.');
                 }
             });
         }
     }
-    
     </script>
 </body>
 
