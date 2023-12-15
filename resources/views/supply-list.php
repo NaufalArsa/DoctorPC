30bcdce6c95f6f0973c24c7fce341851ef8d4738
NaufalArsa <naufal.arsa.27@gmail.com>
2023-12-16 05:25:53 +0700

    finishing setup for supply page

--- resources/views/supply-list.php
+++ resources/views/supply-list.php
@@ -27,8 +27,6 @@ if(isset($_GET['add'])) {
 $dataCatLaptop = $controller->getDataSupplyLaptop();
 $dataCatDesktop = $controller->getDataSupplyDesktop();
 $dataCatGadget = $controller->getDataSupplyGadget();
-    // $controller->displaySupplyPage();
-
 
 ?>
 
@@ -53,7 +51,8 @@ $dataCatGadget = $controller->getDataSupplyGadget();
 <body>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <!-- Category Preview -->
+   
+    <!-- Category Section -->
 
     <div class="h-screen">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -64,7 +63,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                     <a data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="p-2 rounded-lg border border-black border-2 hover:bg-black hover:text-white hover:shadow-lg" id="sellOption" href="#sellItem">Want to sell your item?</a>
                 </div>
 
-                <!-- Banner -->
+                <!-- Category Content -->
 
                 <div class="">
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -73,7 +72,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                     <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
                         <div class="group relative">
                         <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="h-full w-full object-cover object-center">
+                            <img src="https://i.pinimg.com/564x/91/2a/83/912a83b197621e8432c6062d96945b56.jpg" alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug." class="h-full w-full object-cover object-center">
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
                             <a id="laptopTrigger" href="#showLaptop">
@@ -85,7 +84,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                         </div>
                         <div class="group relative">
                         <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
+                            <img src="https://i.pinimg.com/564x/5d/6c/bc/5d6cbcc1410cbcd8831505e3dbd17d82.jpg" alt="Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant." class="h-full w-full object-cover object-center">
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
                             <a id="desktopTrigger" href="#showDesktop">
@@ -93,19 +92,19 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                             PC Desktop
                             </a>
                         </h3>
-                        <p class="text-base font-semibold text-gray-900">Journals and note-taking</p>
+                        <p class="text-base font-semibold text-gray-900">Fulfill your Desktop needs</p>
                         </div>
                         <div class="group relative">
                         <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="h-full w-full object-cover object-center">
+                            <img src="https://i.pinimg.com/564x/b3/67/81/b3678119570da400ff2cd5765f28507b.jpg" alt="Collection of four insulated travel bottles on wooden shelf." class="h-full w-full object-cover object-center">
                         </div>
                         <h3 class="mt-6 text-sm text-gray-500">
                             <a id="gadgetTrigger" href="#showGadget">
                             <span class="absolute inset-0"></span>
-                            Travel
+                            Gadget
                             </a>
                         </h3>
-                        <p class="text-base font-semibold text-gray-900">Daily commute essentials</p>
+                        <p class="text-base font-semibold text-gray-900">Grab the helpfull gadget to make your life easier</p>
                         </div>
                     </div>
                     </div>
@@ -115,6 +114,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                 <!-- ... -->
 
 
+                <!-- Data Laptop -->
 
                 <div class="mt-6 grid grid-cols-3 gap-y-6 gap-x-6 space-y-0 px-8">
         
@@ -177,10 +177,11 @@ $dataCatGadget = $controller->getDataSupplyGadget();
         
                 </div>
 
+                <!--  -->
+
                 <!-- Data Desktop -->
 
                 <div class="mt-6 grid grid-cols-3 gap-y-6 gap-x-6 space-y-0">
-                        
                                     
                         <?php 
 
@@ -214,7 +215,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                                         ?>
                                         
                                         <p class="mt-3 max-w-max text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 font-medium rounded-lg text-sm p-2">Desktop</button>
-                                        <!-- <p class="font-normal text-gray-700">Category: Laptop</p> -->
+                                    
                                     </div>
                                     <a href="buy-supply.php?act=showPreview&id=<?= $row["ID_BARANG"]; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                         Show preview
@@ -237,9 +238,10 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                         
                         ?>
 
-                <!-- ... -->
 
             </div>
+
+            <!-- ... -->
 
             <!-- Data Gadget -->
 
@@ -277,12 +279,9 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                                     ?>
                                     
                                     <p class="mt-3 max-w-max text-white bg-gradient-to-br from-pink-500 to-orange-400 font-medium rounded-lg text-sm p-2">Gadget</button>
-                                    <!-- <p class="font-normal text-gray-700">Category: Laptop</p> -->
+                               
                                 </div>
 
-                                <!-- <form action="?showPreview&id=" method="GET">
-                                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Default</button>
-                                </form> -->
                                 <a href="buy-supply.php?act=showPreview&id=<?= $row["ID_BARANG"]; ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                     Show preview
                                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -338,11 +337,11 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                         </div>
                         <div class="col-span-2">
                             <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Price</label>
-                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="$2999" required>
+                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="150000" required>
                         </div>
                         <div class="col-span-2 sm:col-span-1">
                             <label for="stock" class="block mb-2 text-sm font-medium text-gray-900">Stock</label>
-                            <input type="number" name="stock" id="stock" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="$2999" required>
+                            <input type="number" name="stock" id="stock" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="50" required>
                         </div>
                         <div class="col-span-2 sm:col-span-1">
                             <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Category</label>
@@ -355,7 +354,7 @@ $dataCatGadget = $controller->getDataSupplyGadget();
                         </div>
                         <div class="col-span-2">
                             <label for="image" class="block mb-2 text-sm font-medium text-gray-900">Product image</label>
-                            <textarea name="image" id="image" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write product description here" required></textarea>                    
+                            <textarea name="image" id="image" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Insert image URL here" required></textarea>                    
                         </div>
                     </div>
                     <button id="sellButton" type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
@@ -366,162 +365,38 @@ $dataCatGadget = $controller->getDataSupplyGadget();
             </div>
         </div>
     </div> 
-
+    <!--  -->
+    
+    
     <!-- Alert -->
-    <div id="sellAlert" class="hidden flex items-center p-4 mb-4 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
-        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-        </svg>
-        <span class="sr-only">Info</span>
-        <div class="ms-3 text-sm font-medium">
-            A simple info alert. Give it a click if you like.
-        </div>
-    </div>
+    
     <!-- ... -->
 
     <script>
-    // const laptopTrigger = document.getElementById('laptopHref');
-    // const laptopButton = document.getElementById('laptopTrigger');
-    // const dataLaptop = document.getElementsByTagName('dataLaptop');
-    // // document.createElement("dataLaptop");
+   
 
-    // const laptopQuickview = document.getElementById('laptopQuickview');
-    // const closeButton = document.getElementById('closeButton');
-    // const supplyButton = document.getElementById('supplyButton');
-    const sellButton = document.getElementById('sellButton');
-    const supplyData = document.getElementsByClassName('supplyData');
+   $(document).ready(function(){
 
-    // sellButton.onclick = function() {
-    //     setTimeout(function () {
-    //         window.location.href = 'supply-list.php#showLaptop'; // Replace with your target page URL
-    //     }, 1000); // Adjust the delay as needed
-    // }
+        $("#laptopTrigger").click(function(){
+            $("dataLaptop").show();
+            $("dataDesktop").hide();    
+            $("dataGadget").hide();
+        });
 
-    // function submitForm(event) {
-    //     // Prevent the default form submission behavior
-    //     event.preventDefault();
+        $("#desktopTrigger").click(function(){
+            $("dataLaptop").hide();
+            $("dataGadget").hide();
+            $("dataDesktop").show();
+        });
 
-    //     // Redirect to another page after a short delay (e.g., 1000 milliseconds)
-    //     setTimeout(function () {
-    //         window.location.href = 'supply-list.php'; // Replace with your target page URL
-    //     }, 1000); // Adjust the delay as needed
-    // }
-
-    
-
-    // // Optional: You can add a function to refresh the page without submitting the form
-    // // function refresh() {
-    // //     window.location.reload();
-    // // }
-
-    // function refresh() {    
-    //     // setTimeout(function () {
-    //     // }, 1000);    
-    //     window.location.replace(window.location.supply-list.php);
-    // }
-
-    // Jquery Function
-    $("#laptopTrigger").click(function(){
-        $("dataLaptop").show();
-        $("dataDesktop").hide();    
-        $("dataGadget").hide();
-    });
-
-    $("#desktopTrigger").click(function(){
-        $("dataLaptop").hide();
-        $("dataGadget").hide();
-        $("dataDesktop").show();
-    });
-
-    $("#gadgetTrigger").click(function(){
-        $("dataLaptop").hide();
-        $("dataGadget").show();
-        $("dataDesktop").hide();
-    });
-
-    // $("#sellButton").click(function(){
-    //     setsetTimeout(function(){
-    //         // location.reload();
-    //         $("#sellAlert").show();
-
-    //     }, 3000);
-
-    //     setTimeout(function(){
-    //         location.reload();
-    //     }, 3000)
+        $("#gadgetTrigger").click(function(){
+            $("dataLaptop").hide();
+            $("dataGadget").show();
+            $("dataDesktop").hide();
+        });
         
-    // });
+    });
 
-    //Alert JavaScript Function 
-
-    // import { Dismiss } from 'flowbite';
-
-    // const $targetEl = document.getElementById('targetElement');
-
-    // // optional trigger element
-    // const $triggerEl = document.getElementById('closeSellButton');
-
-    // function hide() {
-    // // options object
-    // const options = {
-    // transition: 'transition-opacity',
-    // duration: 1000,
-    // timing: 'ease-out',
-
-    // // callback functions
-    // onHide: (context, targetEl) => {
-    //     console.log('element has been dismissed')
-    //     console.log(targetEl)
-    // }
-    // };
-
-    // // instance options object
-    // const instanceOptions = {
-    // id: 'targetElement',
-    // override: true
-    // };
-
-    // const dismiss = new Dismiss($targetEl, $triggerEl, options, instanceOptions);
-
-    // }
-    // // dismiss.hide();
-
-
-    // $(document).ready(function() {
-    //     $("#previewButton").click(function() {
-    //         var itemId = $(this).data("id");
-
-    //         // Use AJAX to fetch data for the selected item
-    //         $.ajax({
-    //             type: "GET",
-    //             url: "?act=showPreview&id=" + itemId,
-    //             success: function(data) {
-    //                 // Update the modal content with the fetched data
-    //                 $("#modalContainer").html(data);
-    //                 $("#dataPreview").show();
-    //             },
-    //             error: function() {
-    //                 alert("Error fetching item data");
-    //             }
-    //         });
-    //     });
-
-    //     $("#closeButton").click(function() {
-    //         $("#dataPreview").hide();
-    //     });
-    // });
-    // laptopButton.onclick = function() {
-    //     dataLaptop.style.display = "block";
-    // }
-
-
-    // laptopTrigger.onclick = function() {
-    //     laptopQuickviews.style.display = "block";
-    // }
-
-    // cb1.onclick = function() {
-    //     laptopQuickviews.style.display = "none";
-    // } -->
 
     </script>
 
