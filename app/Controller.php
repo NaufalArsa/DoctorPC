1d97c753011e2fdec0c4e6f366c55105fff0325d
NaufalArsa <naufal.arsa.27@gmail.com>
2023-12-16 14:31:31 +0700

    add Sell and Buy Supply business logic

--- app/Controller.php
+++ app/Controller.php
@@ -59,6 +59,47 @@ class Controller {
     public function removeMechanic($id) {
         $this->model->deleteMechanic($id);
     }
+
+    public function showrequest(){
+        $dataReq = $this->model->showrequest();
+        include("../resources/views/service-request.php");
+    }
+
+    public function getDataSupply() {
+        return $this->model->getDataSupply();
+    }   
+
+    public function getDataSupplyLaptop() {
+        return $this->model->getDataSupplyLaptop();
+    }
+
+    public function getDataSupplyDesktop() {
+        return $this->model->getDataSupplyDesktop();
+    }
+
+    public function getDataSupplyGadget() {
+        return $this->model->getDataSupplyGadget();
+    }
+
+    public function getDataPreview($id) {
+        return $this->model->getDataPreview($id);
+    }
+
+    public function addData($name, $price, $stock, $idcategory, $image) {
+        $this->model->addData($name, $price, $stock, $idcategory, $image);
+    }
+
+    public function displaySupplyPage() {
+        include("supply-list.php");
+    }
+
+    public function updateData($id, $stock) {
+        $this->model->updateData($id, $stock);
+    }   
+
+    public function deleteData($id) {
+        $this->model->deleteData($id);
+    }   
 }
 
 ?>
