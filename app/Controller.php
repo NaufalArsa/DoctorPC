483dc6a7e1b626efc4be48f496890f28fcb7e147
NaufalArsa <naufal.arsa.27@gmail.com>
2023-12-16 14:51:34 +0700

    add Profile Personalization feature

--- app/Controller.php
+++ app/Controller.php
@@ -98,6 +98,33 @@ class Controller {
     public function deleteData($id) {
         $this->model->deleteData($id);
     }   
+
+    public function editProfile($currentPassword, $newUsername, $newEmail, $newPassword)
+    {
+        $username = $_SESSION['username'];
+        $result = $this->model->checkPassword($username, $currentPassword);
+
+        if ($result) {
+            $this->model->updateUsername($username, $newUsername);
+
+            if (!empty($newEmail)) {
+                $this->model->updateEmail($username, $newEmail);
+            }
+
+            if (!empty($newPassword)) {
+                $this->model->updatePassword($username, $newPassword);
+            }
+
+            $_SESSION['username'] = $newUsername;
+            $_SESSION['update_success'] = true;
+            header("location: ../dashboard.php");
+            exit();
+        } else {
+            $_SESSION['update_error'] = "Current password is incorrect.";
+            header("location: editProfil.php");
+            exit();
+        }
+    }
 }
 
 ?>
